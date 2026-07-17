21766fc0455247bf07482c7219d27de6a4310fb2
Yair Orttega <y.ortega.316197595@gmail.com>
2026-07-16 18:32:51 -0600

    chore: use SafeAreaView from react-native-safe-area-context

--- src/screens/BluetoothScreen.js
+++ src/screens/BluetoothScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, SafeAreaView, PermissionsAndroid, Platform } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, PermissionsAndroid, Platform } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { LinearGradient } from 'expo-linear-gradient';
 import { useAppStore } from '../store/store';
 import { bleService } from '../services/bleManager';
